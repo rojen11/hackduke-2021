c88405d4f1d32e8c29c8859b38ab2d6a71f4da46
PratikSilwal5 <pratiksilwal5@gmail.com>
2021-10-24 04:29:47 -0400

    remainder page fill

--- frontend/src/components/ReminderCard/reminderCard.js
+++ frontend/src/components/ReminderCard/reminderCard.js
@@ -1,62 +1,60 @@
-import style from './reminderCard.module.scss';
+import style from "./reminderCard.module.scss";
+
+let cardData = [
+  ["Take Buddy to Vet", "on October 28, 2021", "Annual Check-up"],
+  [
+    "Grooming Day",
+    "on November 03, 2021",
+    "Appointment at Pet's Grooming Center at 10AM",
+  ],
+  [
+    "Playdate with Tucker",
+    "on November 28, 2021",
+    "Time to meet my best friend Tucker :D",
+  ],
+];
 
 export default function ReminderCard() {
-    return (
-        <div className={style.reminderCards}>
-            <div className={style.card}>
-                <span className={style.cardTitle}>
-                    <h3>Take Buddy to Vet</h3>
-                </span>
- 
-                <div className={style.cardReminderDate}>
-                <span className={style.cardTitle}>
-                    <p>on October 28, 2021</p>
-                </span>
-                </div>
+  return (
+    <div className={style.reminderCards}>
+      {cardData.map((e) => (
+        <RemainderCard title={e[0]} date={e[1]} description={e[2]} />
+      ))}
+      ;
+      {cardData.map((e) => (
+        <RemainderCard title={e[0]} date={e[1]} description={e[2]} />
+      ))}
+      ;
+      {cardData.map((e) => (
+        <RemainderCard title={e[0]} date={e[1]} description={e[2]} />
+      ))}
+      ;
+      {cardData.map((e) => (
+        <RemainderCard title={e[0]} date={e[1]} description={e[2]} />
+      ))}
+      ;
+    </div>
+  );
+}
 
-                <div className={style.cardContent}>
-                <span className={style.cardTitle}>
-                    <p>Annual Check-up</p>
-                </span>
-                </div>
-            </div>
+function RemainderCard({ title, date, description }) {
+  return (
+    <div className={style.card}>
+      <span className={style.cardTitle}>
+        <h3>{title}</h3>
+      </span>
 
-            <div className={style.card}>
-                <span className={style.cardTitle}>
-                    <h3>Grooming Day</h3>
-                </span>
- 
-                <div className={style.cardReminderDate}>
-                <span className={style.cardTitle}>
-                    <p>on November 03, 2021</p>
-                </span>
-                </div>
+      <div className={style.cardReminderDate}>
+        <span className={style.cardTitle}>
+          <p>{date}</p>
+        </span>
+      </div>
 
-                <div className={style.cardContent}>
-                <span className={style.cardTitle}>
-                    <p>Appointment at Pet's Grooming Center at 10AM</p>
-                </span>
-                </div>
-            </div>
-
-            <div className={style.card}>
-                <span className={style.cardTitle}>
-                    <h3>Playdate with Tucker</h3>
-                </span>
- 
-                <div className={style.cardReminderDate}>
-                <span className={style.cardTitle}>
-                    <p>on November 28, 2021</p>
-                </span>
-                </div>
-
-                <div className={style.cardContent}>
-                <span className={style.cardTitle}>
-                    <p>Time to meet my best friend Tucker :D </p>
-                </span>
-                </div>
-            </div>
-            
-        </div>
-    )
+      <div className={style.cardContent}>
+        <span className={style.cardTitle}>
+          <p>{description}</p>
+        </span>
+      </div>
+    </div>
+  );
 }
